1a602c793ea69056c5a67f61accacb33550f7204
snickalls <socketdesignlab@gmail.com>
2018-11-02 18:54:52 +1300

    graphics working

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,16 +1,22 @@
    import java.awt.Color;
+   import javax.swing.Timer;
    import java.awt.event.ActionEvent;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
    import javax.swing.JPanel;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
 	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	   private final static int TIMER_DELAY = 5;
 	      
 	   public PongPanel() {
 	       setBackground(BACKGROUND_COLOUR);
+	       Timer timer = new Timer(TIMER_DELAY, this);
+	       timer.start();
 	   }
 
   
@@ -28,6 +34,19 @@
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
+      }
+      
+      public void update() {
+    	  
+      }
+      
+      @Override
+      public void paintComponent(Graphics g) {
+    	  super.paintComponent(g);
+    	  g.setColor(Color.WHITE);
+    	  g.fillRect(20, 20, 100, 100);
+    	  
       }
   
  }
